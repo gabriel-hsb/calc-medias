6a282a9bd5c97bbd0591d2be414398a21cb3625c
Gabriel <ghbranco6@gmail.com>
2024-03-13 13:34:31 -0300

    Added styles for the html

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+const mediaAprovar = window.prompt("Média mínima para aprovação");
 const form = document.querySelector("form");
 const submit = document.querySelector("form button");
 const table = document.querySelector("table");
 
-const mediaAprovar = window.prompt("Média mínima para aprovação");
+let notas = [];
+let atividades = [];
 let linha = "";
 
 // quando clicar em submit, chama todas as funções criadas
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   adicionarLinha();
   atualizaTabela();
 });
 
+
+// para conseguir a média final = 
+// usar foreach no notas, somar notas e
+// dividir pelo i (do index (i) do foreach)
+// 
+
+function mediaFinal (){
+  
+}
+ 
 function adicionarLinha() {
   const nomeAtividade = document.querySelector("#nome-atividade").value;
   const notaAtividade = document.querySelector("#nota-atividade").value;
   const aprovadoAtividade = notaAtividade >= mediaAprovar;
   let statusAtividade = "";
   if (aprovadoAtividade) {
-    statusAtividade = `<td class="aprovado"> Aprovado! </td>`;
+    statusAtividade = `<td class="aprovado"> Aprovado</td>`;
   } else {
-    statusAtividade = "Reprovado!";
+    statusAtividade = `<td class="reprovado"> Reprovado</td>`;
   }
 
   linha += "<tr>";
@@ -31,6 +43,8 @@ function adicionarLinha() {
 
   linha += "</tr>";
 }
+
+
 
 // Insere a linha da função adicionarLinha
 // dentro do body da tabela utiilizando
